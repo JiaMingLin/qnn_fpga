f21aa8df6f173f14db04cd0e24a3e83e41db5a95
ron <robin890650@gmail.com>
2024-01-05 22:44:13 +0800

    mnist

--- linear.py
+++ linear.py
@@ -119,9 +119,7 @@ class Linear(Module):
             tmp[x<0] = -1
             return tmp
 
-        self.weight = sign(self.weight)
-
-        return F.linear(input, self.weight, self.bias)
+        return F.linear(input, sign(self.weight), self.bias)
 
     def extra_repr(self) -> str:
         return f'in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}'
