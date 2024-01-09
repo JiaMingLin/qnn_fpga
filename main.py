b8b78c47f9f10606a6c7875c333a106358c7d4b5
ron <robin890650@gmail.com>
2024-01-09 09:47:59 +0800

    MNIST

--- main.py
+++ main.py
@@ -13,14 +13,15 @@ torch.manual_seed(0)
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = Conv2d(1, 32, 3, 1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = Conv2d(32, 64, 3, 1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = Linear(9216, 128)
-        self.fc2 = Linear(128, 10)
+
+        self.fc1 = Linear(784, 2048)
+        self.bn1 = nn.BatchNorm1d(2048)
+        self.fc2 = Linear(2048, 2048)
+        self.bn2 = nn.BatchNorm1d(2048)
+        self.fc3 = Linear(2048, 2048)
+        self.bn3 = nn.BatchNorm1d(2048)
+        self.fc4 = Linear(2048, 10)
+        self.bn3 = nn.BatchNorm1d(10)
         self.hardtanh = nn.Hardtanh()
 
     def sign(self, x):
@@ -29,20 +30,23 @@ class Net(nn.Module):
         return tmp
 
     def forward(self, x):
-        x = self.conv1(x)
+
+        x = torch.flatten(x)
+        x = self.fc1(x)
         x = self.bn1(x)
         x = self.hardtanh(x)
-        x = self.sign(x)
-        x = self.conv2(x)
+
+        x = self.fc2(x)
         x = self.bn2(x)
         x = self.hardtanh(x)
-        x = self.sign(x)
-        x = F.max_pool2d(x, 2)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
+
+        x = self.fc3(x)
+        x = self.bn3(x)
         x = self.hardtanh(x)
-        x = self.sign(x)
-        x = self.fc2(x)
+
+        x = self.fc4(x)
+        x = self.bn4(x)
+
         output = F.log_softmax(x, dim=1)
         return output
 
