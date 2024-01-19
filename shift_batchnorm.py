ec40a20403520e0bd3bef8bbd1d8c79e89b223d3
JiaMingLin <robin890650@gmail.com>
2024-01-19 21:53:00 +0800

    shift_batchnorm

--- shift_batchnorm.py
+++ shift_batchnorm.py
@@ -12,17 +12,20 @@ class ShiftBatchNorm(nn.Module):
         self.gamma = torch.nn.Parameter(data=torch.ones(size))
         self.register_parameter('beta', self.beta)
         self.register_parameter('gamma', self.gamma)
-        self.running_mean = torch.zeros(size)
-        self.running_var = torch.ones(size)
+        self.running_mean = torch.zeros(size).to('cuda')
+        self.running_var = torch.ones(size).to('cuda')
         self.momentum = momentum
 
     def forward(self, x):
-        batch_mean = torch.mean(x, dim=1)
-        batch_var = torch.var(x, unbiased=False, dim=1)
+        batch_mean = torch.mean(x, dim=0)
+        batch_var = torch.var(x, unbiased=False, dim=0)
         
         def ap2(x):
             return torch.sign(x) * (2**(torch.round(torch.log2(torch.abs(x)))))
         
+        #print(x.shape)
+        #print(self.running_mean.shape)
+        #print(batch_mean.shape)
         self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * batch_mean
         self.running_var = self.momentum * self.running_var + (1-self.momentum) * batch_var
         
