e228dc62e9127369d2062d80328d6edf22ee464c
ron <robin890650@gmail.com>
2024-01-20 22:46:15 +0800

    shift_batchnorm

--- shift_batchnorm.py
+++ shift_batchnorm.py
@@ -8,31 +8,24 @@ class ShiftBatchNorm(nn.Module):
 
         # Trainable parameters
         self.size = size
-        self.beta = torch.nn.Parameter(data=torch.zeros(size))
-        self.gamma = torch.nn.Parameter(data=torch.ones(size))
-        self.register_parameter('beta', self.beta)
-        self.register_parameter('gamma', self.gamma)
-        self.running_mean = torch.zeros(size).cuda()
-        self.running_var = torch.ones(size).cuda()
-        self.register_buffer('moving_mean', self.running_mean)
-        self.register_buffer('moving_var', self.running_var)
+        self.register_parameter('beta', torch.nn.Parameter(data=torch.zeros(size)))
+        self.register_parameter('gamma', torch.nn.Parameter(data=torch.ones(size)))
+        self.register_buffer('running_mean', torch.zeros(size))
+        self.register_buffer('running_var', torch.ones(size))
         self.momentum = momentum
 
     def forward(self, x):
         batch_mean = torch.mean(x, dim=0)
         batch_var = torch.var(x, unbiased=False, dim=0)
-        
+
         def ap2(x):
             return torch.sign(x) * (2**(torch.round(torch.log2(torch.abs(x)))))
-        
-        #print(x.shape)
-        #print(self.running_mean.shape)
-        #print(batch_mean.shape)
+
         self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * batch_mean
         self.running_var = self.momentum * self.running_var + (1-self.momentum) * batch_var
-        
+
         x_hat = (x - batch_mean) / ap2(torch.sqrt(batch_var + self.epsilon))
-#         x_hat = (x - batch_mean) / torch.sqrt(batch_var + self.epsilon)
+        
         result = x_hat * ap2(self.gamma) + self.beta
 
         return result
