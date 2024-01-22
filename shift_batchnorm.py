9a9f8a47c1ad7e0a5890ef33fb6aba4dd022ea1b
ron <robin890650@gmail.com>
2024-01-22 09:56:09 +0800

    shift_batchnorm

--- shift_batchnorm.py
+++ shift_batchnorm.py
@@ -15,17 +15,23 @@ class ShiftBatchNorm(nn.Module):
         self.momentum = momentum
 
     def forward(self, x):
-        batch_mean = torch.mean(x, dim=0)
-        batch_var = torch.var(x, unbiased=False, dim=0)
 
         def ap2(x):
             return torch.sign(x) * (2**(torch.round(torch.log2(torch.abs(x)))))
 
-        self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * batch_mean
-        self.running_var = self.momentum * self.running_var + (1-self.momentum) * batch_var
+        result = None
+        if self.training: # training
+            batch_mean = torch.mean(x, dim=0)
+            batch_var = torch.var(x, unbiased=False, dim=0)
 
-        x_hat = (x - batch_mean) / ap2(torch.sqrt(batch_var + self.epsilon))
+            self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * batch_mean
+            self.running_var = self.momentum * self.running_var + (1-self.momentum) * batch_var
+
+            x_hat = (x - batch_mean) / ap2(torch.sqrt(batch_var + self.epsilon))
         
-        result = x_hat * ap2(self.gamma) + self.beta
+            result = x_hat * ap2(self.gamma) + self.beta
+        else: # inferencing
+            x_hat = (x - self.running_mean) / ap2(torch.sqrt(self.running_var + self.epsilon))
+            result = x_hat * ap2(self.gamma) + self.beta
 
         return result
